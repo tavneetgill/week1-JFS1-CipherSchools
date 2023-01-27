008369d07c9495a4f1479029d37394936107bcd7
Tavneet Kaur Gill <71913873+tavneetgill@users.noreply.github.com>
2023-01-27 22:14:48 +0530

    Add files via upload

---
+++ employee.java
@@ -0,0 +1,9 @@
+
+public class employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ encapsulation.java
@@ -0,0 +1,9 @@
+
+public class encapsulation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
